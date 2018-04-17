5ea36bf1c6a8b09d79c75beeec412d12e1ff952e
huang-zp <hzhangpeng@gmail.com>
2018-04-17 14:53:53 +0800

    update

--- app/controllers/index_route.py
+++ app/controllers/index_route.py
@@ -5,7 +5,7 @@ from sqlalchemy import func
 from datetime import datetime, timedelta
 
 now_time = datetime.now()
-begin_time = now_time + timedelta(days=7)
+begin_time = now_time + timedelta(days=-7)
 
 aj = Blueprint('aj', __name__, url_prefix='')
 param_location = ('json', )
@@ -13,5 +13,6 @@ param_location = ('json', )
 
 @aj.route('/')
 def index():
+    data = [22222, 12503, 17177, 19658, 17031, 19931, 17133, 4175]
 
     return render_template('index.html', data=data)
